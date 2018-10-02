5499a8f9af8855d2a13d9e1c05af4acf1027f0dc
Andrey Sergeev <andrey@chyuck.com>
2018-10-02 17:01:41 -0400

    Implemented Logger

---
+++ Agero.Core.SplunkLogger/ILogger.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+
+namespace Agero.Core.SplunkLogger
+{
+    /// <summary>Splunk Logger</summary>
+    public interface ILogger
+    {
+        /// <summary>Submits log to Splunk</summary>
+        /// <param name="type">Log type (Error, Info, etc.)</param>
+        /// <param name="message">Log text message</param>
+        /// <param name="data">Any object which serialized into JSON</param>
+        /// <param name="correlationId">Any optional string which can correlate different logs</param>
+        /// <returns>Returns flag whether log submitted to Splunk</returns>
+        bool Log(string type, string message, object data = null, string correlationId = null);
+
+        /// <summary>Submits log to Splunk</summary>
+        /// <param name="type">Log type (Error, Info, etc.)</param>
+        /// <param name="message">Log text message</param>
+        /// <param name="data">Any object which serialized into JSON</param>
+        /// <param name="correlationId">Any optional string which can correlate different logs</param>
+        /// <returns>Returns flag whether log submitted to Splunk</returns>
+        Task<bool> LogAsync(string type, string message, object data = null, string correlationId = null);
+    }
+}
